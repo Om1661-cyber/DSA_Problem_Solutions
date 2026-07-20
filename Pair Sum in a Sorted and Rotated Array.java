8b819907d11d706a5a481850dfb86bfb6cc54417
Om Sonawane <omsonawane1661@gmail.com>
2026-07-20 10:40:09 +0530

    Pair Sum in a Sorted and Rotated Array

---
+++ Pair Sum in a Sorted and Rotated Array.java
@@ -0,0 +1,36 @@
+
+class Solution {
+    static boolean pairInSortedRotated(int arr[], int target) {
+        // Your code here
+        int n = arr.length;
+        int pivot = -1;
+        
+        for(int i = 0; i < n -1; i++){
+            if(arr[i] > arr[i+1]){
+                pivot = i;
+                break;
+            }
+        }
+        
+        if(pivot == -1){
+            pivot = n -1;
+        }
+        
+        int left = (pivot + 1) % n;
+        int right = pivot;
+        
+        while(left != right){
+            int sum = arr[left] + arr[right];
+            
+            if(sum == target){
+                return true;
+            }
+            if(sum < target){
+                left = (left + 1)% n;
+            }else{
+                right = (n + right - 1)% n;
+            }
+        }
+        return false;
+    }
+}
