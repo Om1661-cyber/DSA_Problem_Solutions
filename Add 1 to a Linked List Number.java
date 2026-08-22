1e3e8d68f6580693cf3f1184303a48c45d5ed702
Om Sonawane <omsonawane1661@gmail.com>
2026-08-22 22:30:06 +0530

    Optimal way

--- Add 1 to a Linked List Number.java
+++ Add 1 to a Linked List Number.java
@@ -10,53 +10,81 @@ class Node{
 }
 */
 class Solution {
-    
-    public Node reverse(Node head){
+    // optimal
+    public int help(Node temp){
+        if(temp == null){
+            return 1;
+        }
+        int c = help(temp.next);
+        temp.data = temp.data + c;
+        if(temp.data < 10){
+            return 0;
+        }
+        temp.data = 0;
+        return 1;
+    }
+     public Node addOne(Node head) {
+         if(head == null){
+             return head;
+         }
+         
+         int c = help(head);
+         if(c == 1){
+             Node nn = new Node(1);
+             nn.next = head;
+             head = nn;
+             
+         }
+             return head;
+     }
+     
+     // Brute Fore 
+    // public Node reverse(Node head){
        
-        Node prev = null;
-        Node curr = head;
-        Node nnext = null;
+    //     Node prev = null;
+    //     Node curr = head;
+    //     Node nnext = null;
         
-        while(curr != null){
-            nnext = curr.next;
-            curr.next = prev;
-            prev = curr;
-            curr = nnext;
+    //     while(curr != null){
+    //         nnext = curr.next;
+    //         curr.next = prev;
+    //         prev = curr;
+    //         curr = nnext;
           
             
-        }
+    //     }
        
-        return prev;
+    //     return prev;
         
-    }
-    public Node addOne(Node head) {
-        // code here.
-        if(head == null){
-            return head;
-        }
-        Node newhead = reverse(head);
-        Node temp = newhead;
-        int c = 1;
-        while(temp != null){
-            if(c == 1){
-                int sum = temp.data + c;
-                temp.data = sum %10;
-                c = sum / 10;
+    // }
+    // public Node addOne(Node head) {
+    //     // code here.
+    //     if(head == null){
+    //         return head;
+    //     }
+    //     Node newhead = reverse(head);
+    //     Node temp = newhead;
+    //     int c = 1;
+    //     while(temp != null){
+    //         if(c == 1){
+    //             int sum = temp.data + c;
+    //             temp.data = sum %10;
+    //             c = sum / 10;
             
                 
-            }else {
-                break;
-            }
-            temp = temp.next;
-        }
-        Node nhead = reverse(newhead);
+    //         }else {
+    //             break;
+    //         }
+    //         temp = temp.next;
+    //     }
+    //     Node nhead = reverse(newhead);
         
-        if(c == 1){
-            Node first = new Node(1);
-            first.next = nhead;
-            nhead = first;
-        }
+    //     if(c == 1){
+    //         Node first = new Node(1);
+    //         first.next = nhead;
+    //         nhead = first;
+    //     }
         
-        return nhead;
-    }
+    //     return nhead;
+    //}
 }
