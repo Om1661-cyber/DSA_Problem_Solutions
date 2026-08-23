e1783374879fbb1ad8e12d0b8ec32dff9b3a2e44
Om Sonawane <omsonawane1661@gmail.com>
2026-08-23 19:11:35 +0530

    Using 1 pointer

--- Reverse Array Using Recursion.java
+++ Reverse Array Using Recursion.java
@@ -1,22 +1,47 @@
+
+// Using 1 Pointer
+
+
 class Solution {
-    public void revUsingRec(int l, int r, int arr[]){
-        if(l >= r){
+    public void revUsingRec(int i, int n, int arr[]){
+        if(i == n/2){
             return ;
         }
-        int temp = arr[l];
-        arr[l] = arr[r];
-        arr[r] = temp;
+        int temp = arr[i];
+        arr[i] = arr[n-i-1];
+        arr[n-i-1] = temp;
         
-        revUsingRec(l+1, r - 1, arr);
+        revUsingRec(i+1, n, arr);
     }
     public void reverseArray(int arr[]) {
         // code here
-      
-           
-            revUsingRec(0, arr.length-1, arr);
-            
-            
         
-        
+        revUsingRec(0, arr.length, arr);
     }
 }
+
+
+// Using 2 Pointer
+
+// class Solution {
+//     public void revUsingRec(int l, int r, int arr[]){
+//         if(l >= r){
+//             return ;
+//         }
+//         int temp = arr[l];
+//         arr[l] = arr[r];
+//         arr[r] = temp;
+        
+//         revUsingRec(l+1, r - 1, arr);
+//     }
+//     public void reverseArray(int arr[]) {
+//         // code here
+      
+           
+//             revUsingRec(0, arr.length-1, arr);
+            
+            
+        
+        
+//     }
+// }
