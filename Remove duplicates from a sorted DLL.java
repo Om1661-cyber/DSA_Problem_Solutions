03a80edcd92c66fcb1c207c28f09381447f2dd15
Om Sonawane <omsonawane1661@gmail.com>
2026-08-31 18:03:14 +0530

    Remove duplicates from a sorted DLL

---
+++ Remove duplicates from a sorted DLL.java
@@ -0,0 +1,37 @@
+/* Structure of a link list node
+class Node {
+    int data;  // value stored in node
+    Node next;
+    Node prev;
+
+    Node(int value) {
+        data = value;
+        next = null;
+        prev = null;
+    }
+}
+*/
+class Solution {
+    Node removeDuplicates(Node headRef) {
+        // code here
+        if(headRef == null && headRef.next == null){
+            return headRef;
+        }
+        Node temp = headRef;
+        
+        
+        while(temp != null && temp.next != null){
+            Node nextnode = temp.next;
+            
+            while(nextnode != null && nextnode.data == temp.data){
+                nextnode = nextnode.next;
+            }
+            temp.next = nextnode;
+            if(nextnode != null) nextnode.prev = temp;
+            
+            temp = nextnode;
+        }
+        
+        return headRef;
+    }
+}
